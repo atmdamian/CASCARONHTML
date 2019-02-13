8e1156b54f42f22667536709f79fba20735bb76b
damian.atanacio <damian.atanacio@RNS-AMD.dominionmx.com.mx>
2019-02-12 18:52:35 -0600

    Merge origin/master
    Conflicts:
    	web/WEB-INF/jsp/catalogos/detallesCatalogo.jsp

--- public_html/js/cargarmenus.js
+++ public_html/js/cargarmenus.js
@@ -3,10 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+function loadIframe(iframeName, url) {
+    if (window.frames[iframeName]) {
+        window.frames[iframeName].location = url;
+        return false;
+    }
+    return true;
+}
 
-$(document).ready(function () {
-    $('.menuContenedor').load('complementos/menu.html');
-});
-$(document).ready(function () {
-    $('.pieContenedor').load('complementos/footer.html');
-});
